5d9f63337ebbcb6b8920325bca6f5fe3f59b7d75
Gabriel Duarte <biel.dbastos@gmail.com>
2022-04-03 14:50:19 -0300

    Segundo commit

--- campoMinado.py
+++ campoMinado.py
@@ -1,15 +1,5 @@
-"""
--Tabuleiro
--click
-lugar da bomba
-numeros descobrindo se ta perto de bomba ou não
-"""
-
 from random import randrange
-from tracemalloc import start
-from venv import create
-
-
+import os
 
 class Game():
 
@@ -17,16 +7,25 @@ class Game():
         self.perdeu = False
         self.createBoard()
         while self.finishGame():
-            self.printBoard()
+            self.printBoard(board=self.board)
             self.chooseCoords()
             self.checkCoords() 
         if self.perdeu == True:
+            os.system('cls')
+            print('='* self.dimensions * 5)
+            self.printBoard(board=self.board)
+            print('')
             print('Você Perdeu!')
+            print('='* self.dimensions * 5)
         else:
+            os.system('cls')
+            print('='* self.dimensions * 5)
+            self.printBoard(board=self.fakeBoard)
+            print('')
             print('Você ganhou!')
+            print('='* self.dimensions * 5)
 
-        
-
+    
     def createBoard(self):
         
         self.scorePositions = 0
@@ -46,7 +45,12 @@ class Game():
         while getRight == False:
             try:
                 self.bombs = int(input('How many bombs? '))
-                getRight = True
+                if self.bombs > self.dimensions:
+                    print(f'The number of bombs cant be higher than the area({self.dimensions})')
+                elif self.bombs <= 0:
+                    print('The number of bombs cant be zero or below it')
+                else:
+                    getRight = True
             except ValueError as error:
                 print('Only numbers!')
 
@@ -81,12 +85,15 @@ class Game():
             try:
                 self.coordX = int(input('Where do you want to play in horizontal?'))
                 self.coordY = int(input('Where do you want to play in vertical?'))
-                if self.board[self.coordX][self.coordY] == '_':
-                    getRight = True
+                if self.coordX >= 0 or self.coordY >= 0:
+                    if self.board[self.coordX][self.coordY] == '_':
+                        getRight = True
+                    else:
+                        print('This positions has already been played')
                 else:
-                    print('This positions has already been played')
+                    print(f'Only positives numbers in range 0 - {self.dimensions - 1}')
             except:
-                print('Write only numbers')
+                print(f'Only positives numbers in range 0 - {self.dimensions - 1}')
 
 
     def checkCoords(self):
@@ -99,17 +106,17 @@ class Game():
 
         elif self.fakeBoard[self.coordX][self.coordY] == 'N' and self.board[self.coordX][self.coordY] == '_':
 
-            for x in range(self.coordX-1, self.coordX+1):
-                if x > 0 and x < self.dimensions:
-                    for y in range(self.coordY-1, self.coordY+1):
-                        if y > 0 and y < self.dimensions:
+            for x in range(self.coordX-1, self.coordX+2):
+                if x >= 0 and x <= self.dimensions - 1:
+                    for y in range(self.coordY-1, self.coordY+2):
+                        if y >= 0 and y <= self.dimensions - 1:
                             if self.fakeBoard[x][y] == 'B':
                                 contBomb += 1
 
             self.board[self.coordX][self.coordY] = contBomb
             self.scorePositions += 1
             
-    def printBoard(self):
+    def printBoard(self, board):
         linha = 0
         print('     ', end='')
         for item in range(self.dimensions):
@@ -119,7 +126,7 @@ class Game():
                 print(f'{item}  ', end='')
         print('=' * (self.dimensions * 4))
             
-        for item in self.board:
+        for item in board:
             print(f'{linha}|   ', end='')
             print ('  '.join(map(str, item)))
             linha += 1
